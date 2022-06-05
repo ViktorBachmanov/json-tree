41618e34cd7464061b9af14585aaec9ecd2ea86a
Vikt0r <vbachmanov@mail.ru>
2022-06-05 20:43:38 +0300

    Add jQuery slideDown/slideUp

--- public/js/tree.js
+++ public/js/tree.js
@@ -2,8 +2,15 @@
     const nodes = document.querySelectorAll(".nodeLabel");
     nodes.forEach((node) => {
         node.addEventListener("click", () => {
-            node.classList.toggle("closed");
-            node.nextElementSibling.classList.toggle("hidden");
+            const nodeContent = node.nextElementSibling;
+            if (node.classList.contains("closed")) {
+                node.classList.remove("closed");
+                $(nodeContent).slideDown();
+            } else {
+                $(nodeContent).slideUp(() => {
+                    node.classList.add("closed");
+                });
+            }
         });
     });
 })();
